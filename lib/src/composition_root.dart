1c0be0ff4b7d59da58cb912c48de3bfc09e22aca
Smartgodswill <ufiagodswill09@gmail.com>
2023-09-08 15:22:34 -0700

    app project started

---
+++ lib/src/composition_root.dart
@@ -0,0 +1,9 @@
+import 'package:design_app/src/smartApp.dart';
+import 'package:flutter/material.dart';
+
+class Composition{
+ static configure(){}
+ static Widget Start(){
+  return   const SmartApp();
+ }
+}
---
+++ lib/src/design_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SmartApp extends StatelessWidget {
+  const SmartApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
